3c7ac1758d2393c49d285aae518bdea5d49adcbb
Soubhagya Nayak <155530626+Soubhagya-Nayak@users.noreply.github.com>
2025-10-24 12:35:43 +0530

    Making Contact Manager Project

---
+++ scm2.0/src/main/java/com/scm/controllers/Pagecontroller.java
@@ -0,0 +1,36 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Pagecontroller {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Welcome to Home.");
+
+        // Sending data to view.
+        model.addAttribute("Name", "Soubhagya Nayak");
+        model.addAttribute("YoutubeChannel", "Learning");
+        model.addAttribute("Githubname", "LifeDrop");
+        model.addAttribute("GitHub", "https://github.com/Soubhagya-Nayak");
+        return "home";
+    }    
+
+    // About Page.
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("Wlcome to about page.");
+        return"about";
+    }
+
+    // Service Page.
+    @RequestMapping("/service")
+    public String servicePage() {
+        System.out.println("Welcome to service page.");
+        return"service";
+    }
+}
